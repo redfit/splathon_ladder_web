21b706caee6fce1618000d479bcac9f37aec0310
daisuke fuchiwaki <daisuke.fuchiwaki@speee.jp>
2021-01-19 21:24:03 +0900

    シーズン６の日程を追加

--- pages/index.vue
+++ pages/index.vue
@@ -14,6 +14,15 @@
         <v-card-subtitle>Splathon Ladder League Portal Page</v-card-subtitle>
         <v-card-text>
           <p>Season6 は 2021/01/16 からスタート！</p>
+          <p>日程</p>
+          <ul>
+            <li>ラウンド1: 1/16 - 1/30</li>
+            <li>ラウンド2: 2/01 - 2/14</li>
+            <li>ラウンド3: 2/15 - 2/28</li>
+            <li>ラウンド4: 3/01 - 3/14</li>
+            <li>ラウンド5: 3/15 - 3/28</li>
+            <li>ラウンド6: 3/29 - 4/11</li>
+          </ul>
         </v-card-text>
       </v-card>
     </v-container>
